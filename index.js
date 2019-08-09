d274b009c5bac112f476b02a49576f24806369e8
kevin Abreu <kabreu11385@gmail.com>
2019-08-09 19:09:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,18 +14,3 @@ function add(a,b) {
   return a / b ; 
 }
 
-function add(a,b) {
-  return a + b ; 
-}
-
-function add(a,b) {
-  return a + b ; 
-}
-
-function add(a,b) {
-  return a + b ; 
-}
-
-function add(a,b) {
-  return a + b ; 
-}
